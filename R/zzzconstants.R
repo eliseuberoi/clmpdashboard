89d7917db3d3da551acc6a85d179e1546809176d
eliseuberoi <57629347+eliseuberoi@users.noreply.github.com>
2021-07-01 08:59:20 +0100

    Update zzzconstants.R

--- R/zzzconstants.R
+++ R/zzzconstants.R
@@ -1,29 +1,40 @@
 ### Package constants
 
-NOBLE_TITLES <- c(
-    "^Lord ",
-    "^Baroness ",
-    "^Viscount ",
-    "^Bishop ",
-    "^Viscountess ",
-    "^Earl ",
-    "^Countess ",
-    "^The Lord ",
-    "^The Earl",
-    "^The Viscount",
-    "^The Viscountess",
-    "^The Countess"
-)
+get_NOBLE_TITLES <- function() {
 
-MP_TITLES <- c(
-    "^Mrs ",
-    "^Ms ",
-    "^Miss ",
-    "^Mr ",
-    "^Dr ",
-    "^Sir ",
-    "^Dame "
-)
+    NOBLE_TITLES <- c(
+        "^Lord ",
+        "^Baroness ",
+        "^Viscount ",
+        "^Bishop ",
+        "^Viscountess ",
+        "^Earl ",
+        "^Countess ",
+        "^The Lord ",
+        "^The Earl",
+        "^The Viscount",
+        "^The Viscountess",
+        "^The Countess"
+    )
+}
+
+get_MP_TITLES <- function() {
+
+    MP_TITLES <- c(
+        "^Mrs ",
+        "^Ms ",
+        "^Miss ",
+        "^Mr ",
+        "^Dr ",
+        "^Sir ",
+        "^Dame "
+    )
+}
+
+
+NOBLE_TITLES <- get_NOBLE_TITLES()
+
+MP_TITLES <- get_MP_TITLES()
 
 MEMBERS <- get_members()
 
